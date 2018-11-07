e5b2865d18b04dc0f9e6d3449dde986eb1594ad6
DimaDontsov <Dimayahoo@mail.ru>
2018-11-08 01:18:29 +0400

    Отформатировал код и добавил отладочную печать в Tree_structs_array

--- flex/flex/tree_structs_array.h
+++ flex/flex/tree_structs_array.h
@@ -4,22 +4,24 @@
 
 #include <stdlib.h>
 
-struct Expr_list_st* createExprList( struct Expression_st* elem)
+struct Expr_list_st* createExprList( struct Expression_st* expr)
 {
     struct Expr_list_st* st = (struct Expr_list_st*) malloc(sizeof(struct Expr_list_st));
-    st->expr = elem;
+    st->expr = expr;
     st->next = NULL;
+    printf("CreateExprList\n");
     return st;
 }
 
-struct Expr_list_st* addToExprList( 
-    struct Expr_list_st* root, struct Expression_st * elem)
+struct Expr_list_st* addToExprList( struct Expr_list_st* list, struct Expression_st * expr)
 {
-    struct Expr_list_st* last = root;
-    while ( last->next != NULL)
+    struct Expr_list_st* last = list;
+    while ( last->next) {
         last = last->next;
-    last->next = createExprList( elem);
-    return root;
+    }
+    last->next = createExprList( expr);
+    printf("AddToExprList\n");
+    return list;
 }
 
 struct Expression_st* createArrayElemCall(
