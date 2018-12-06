2be336a9ef38374cbc9b95385d34233b9341de91
DimaDontsov <Dimayahoo@mail.ru>
2018-12-06 08:27:32 +0400

    коммит для мёрджа

--- flex/flex/test.m
+++ flex/flex/test.m
@@ -1,11 +1,13 @@
 @interface MyClass 
 {} 
 + (int) staticMethod;
++ (int) staticMethod;
 - (float) localMethod;
 @end
 
 int main()
 {
 	MyClass* myObject;
+	myObject->am;
 	[myObject localMethod];
 }
