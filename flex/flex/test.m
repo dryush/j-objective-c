17779c98e699b2b8c61a101ce5d987f68e9a07cb
DimaDontsov <Dimayahoo@mail.ru>
2018-12-25 10:20:20 +0400

    Затестил массивы float

--- flex/flex/test.m
+++ flex/flex/test.m
@@ -1,14 +1,20 @@
-int func() {
-	printf("Fooo");
-	return 1;
+void func (float a[], int size) {
+	int i = 0;
+	printf("This is begin func");
+	while (i < size) {
+		if (a[i] < 0) {
+			printf("This is func");
+			a[i] = 0;
+		}
+		i = i + 1;
+	}
 }
 void main()
 {
 	NSScanner *in = [[NSScanner alloc]init];
-	int c = [ in scanInt];
-	while ( c > 0){
-		return;
-		c = c - 1;
-		printf([NSString fromInt: c]);
-	}
+	float a[5];
+	a[0] = [in scanFloat];
+	int b = !(a[0] > 0 && a[0] < 3.333);
+	printf([NSString fromInt: b]);
+	
 }
